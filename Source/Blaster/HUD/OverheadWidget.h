3750aed6090af2cba79805e49ee591d075089372
Kaan Kirant <kaankirant98@gmail.com>
2024-07-13 01:40:35 +0100

    added functions to display text

--- Source/Blaster/HUD/OverheadWidget.h
+++ Source/Blaster/HUD/OverheadWidget.h
@@ -13,5 +13,16 @@ UCLASS()
 class BLASTER_API UOverheadWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	UPROPERTY(meta=(BindWidget))
+	class UTextBlock* DisplayText;
+
+	void SetDisplayText(FString TextToDisplay);
+
+	UFUNCTION(BlueprintCallable)
+	void ShowPlayerNetRole(APawn* InPawn);
+
+protected:
+	virtual void NativeDestruct() override;
 	
 };
